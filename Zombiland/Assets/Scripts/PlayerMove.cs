7dc5342e714bcd3060af460662798e24c8bdca6c
Alex Labrador <77779042+AlexLabrador@users.noreply.github.com>
2022-01-22 16:01:26 +0100

    skybox asset

--- Zombiland/Assets/Scripts/PlayerMove.cs
+++ Zombiland/Assets/Scripts/PlayerMove.cs
@@ -7,18 +7,53 @@ public class PlayerMove : MonoBehaviour
     //velocidad desplazamiento
     float speed;
     Vector3 dir;
+    float strafe;
+    //characterController
     CharacterController character;
+    //Animator
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
+       
         character = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
         speed = 2.5f;
+        Correr();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        Correr();
+    }
+    void Caminar()
+    {
+        speed = 2.5f;
         dir = transform.TransformDirection(Vector3.forward);
         character.SimpleMove(dir * speed);
+
+        
+    }
+    void Strafe()
+    {
+        speed = 1.2f;
+        dir = transform.TransformDirection(Vector3.right);
+        character.SimpleMove(dir * speed);
+
+    }
+    void Correr()
+    {
+        animator.SetBool("Run", true);
+        speed = 5f;
+        dir = transform.TransformDirection(Vector3.forward);
+        character.SimpleMove(dir * speed);
+
+    }
+    void Girar()
+    {
+
     }
 }
